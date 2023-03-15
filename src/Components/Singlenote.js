aa9f8b994c61104f1b878d015297e17a05e7a896
prajwalkumar05 <prajwalkumaredu8@gmail.com>
2023-03-15 23:43:12 +0530

    change card color

--- src/Components/Singlenote.js
+++ src/Components/Singlenote.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Singlenote = () => {
   return (
-    <div className='h-auto w-[240px] p-4 m-4 rounded bg-red-500'>
+    <div className='h-auto w-[240px] p-4 m-4 rounded bg-[#dee2e6]'>
         <h1 className='text-xl pb-2 font-medium'>Create</h1>
-        <p>create or store any important things in the this web app</p>
+        <p>create or store any important things in the this web app the react is one of the best library of javascript</p>
     </div>
   )
 }
